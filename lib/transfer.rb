f37937dcad8b8cdef33d1e4916d025b3670cd3ba
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 14:01:20 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+    
+    # shouldn't execute if sender or receiver has an invalid account
+    if self.sender.valid? != true || self.receiver.valid? != true
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    # shouldn't execute if the sender's balance is less than the transfer amount
+    elsif self.sender.balance < amount
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    # as long as the transaction hasn't been completed yet...
+    elsif self.status != "complete"
+      self.sender.deposit(-amount)
+      self.receiver.deposit(amount)
+      self.status = "complete"
+    end
+    
+  end
+  
+  def reverse_transfer
+    # should only execute on transfers that have already been completed
+    if self.status == "complete"
+      self.sender.deposit(amount)
+      self.receiver.deposit(-amount)
+      self.status = "reversed"
+    end
+  end
+  
+  
 end
